5177097c891fa43677d1c4dc431d3681040bbbee
Mr Nobody <62615599+Mr-Nobody-Js@users.noreply.github.com>
2021-09-22 00:07:07 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bas
+
+echo "Script criado por @nobodyhell666"
+sleep 1
+
+function download {
+  cd ~/
+  wget https://raw.githubusercontent.com/Mr-Nobody-Js/archives/main/AlterarData.sh
+  wget https://raw.githubusercontent.com/Mr-Nobody-Js/archives/main/criarusuario.sh
+  wget https://raw.githubusercontent.com/Mr-Nobody-Js/archives/main/remover.sh
+  chmod +x *
+}
+
+
